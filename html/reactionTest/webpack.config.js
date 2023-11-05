36a2e5bf40395a8aa71fbf360326d6deee70e8dc
newbe01 <pkwaa3@gmail.com>
2023-11-05 11:36:29 +0900

    webpack-dev-server
    webpack-dev-server를 사용해서
    새로고침이나 , 빌드없이 파일수정시 바로 확인할수 있도록함

--- html/reactionTest/webpack.config.js
+++ html/reactionTest/webpack.config.js
@@ -5,6 +5,12 @@ const path = require("path");
 module.exports = {
   mode: 'development',
   devtool: 'eval',
+  devServer: {
+    port: 8080,
+    hot: true,
+    devMiddleware: {publicPath: "/dist"},
+    static: {directory: path.resolve(__dirname)}
+  },
   resolve: {
     extensions: ['.js', '.vue']
   },
@@ -30,5 +36,6 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.join(__dirname, "dist"),
+    publicPath: "/dist",
   },
 };
