16bcfd27168eb5092238eb60069c08fa21ae61f7
newbe01 <pkwaa3@gmail.com>
2023-11-05 10:41:34 +0900

    reactionTest template

---
+++ html/reactionTest/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import reactionTest from './reactionTest';
+
+new Vue(reactionTest).$mount('#root')
---
+++ html/reactionTest/reactionTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+  
+<style scoped>
+/* 컴포넌트 스타일 */
+</style>
+  
