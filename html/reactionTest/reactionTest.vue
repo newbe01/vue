79e59759e80d9ca5e48ccd34499b48084b0d146f
newbe01 <pkwaa3@gmail.com>
2023-11-05 10:51:10 +0900

    no message

--- html/reactionTest/reactionTest.vue
+++ html/reactionTest/reactionTest.vue
@@ -1,11 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <div id="screen"> {{ message }} </div>
+    <div>
+      <div> 평균 시간 : {{  }}</div>
+      <!-- <button v-on:click="onReset"> reset </button> -->
+      <button @click="onReset"> reset </button>
+    </div>
+  </div>
 </template>
   
 <script>
 export default {
   data() {
-    return {};
+    return {
+      result: [],
+      state: 'wating',
+      message: 'click to start',
+    };
   },
   methods: {},
 };
