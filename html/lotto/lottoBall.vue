0e18e5d8510e52f5b81db5f936d28168a3658183
newbe01 <pkwaa3@gmail.com>
2023-11-06 13:53:28 +0900

    lotto game
    watch 는 데이터의 변경을 감지한다. 사용을 피하도록 노력
    props 는 vue간의 상속관계를 만들어 쓸 수 있다

--- html/lotto/lottoBall.vue
+++ html/lotto/lottoBall.vue
@@ -1,11 +1,49 @@
 <template>
-    
+    <div class="ball" :style=" styleObj "> {{ number }} </div>
 </template>
 
 <script>
+    export default {
+        name : "lottoBall", 
+        props: {
+            number: Number,
+        },
+        methods: {
+            
+        },
+        computed: {
+            styleObj() {
+                let background;
+                if(this.number < 10) {
+                    background = 'red';
+                } else if (this.number <= 20) {
+                    background = 'orange';
+                } else if (this.number <= 30) {
+                    background = 'yellow';
+                } else if (this.number <= 40) {
+                    background = 'green';
+                } else { 
+                    background = 'blue';
+                }
 
+                return {
+                    background,
+                };
+            }
+        },
+    }
 </script>
 
 <style scoped>
-
+    .ball {
+        display: inline-block;
+        border: 1px solid black;
+        border-radius: 20px;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+        margin-right: 20px;
+    }
 </style>
--- html/lotto/lottoGenerator.vue
+++ html/lotto/lottoGenerator.vue
@@ -1,17 +1,80 @@
 <template>
-  <div></div>
+  <div>
+    <div>winning numbers</div>
+    <div id="result">
+      <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball"></lotto-ball>
+    </div>
+    <div> bonus </div>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="redo" @click="onClickRedo"> next game </button>
+  </div>
 </template>
   
 <script>
+import lottoBall from './lottoBall.vue'
+  function getWinNumbers() {
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+      shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+  }
+
+  const timeout =[];
 export default {
+  components: {
+    'lotto-ball' : lottoBall
+  },
   data() {
-    return {};
+    return {
+      winNumbers : getWinNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo : false,
+    };
   },
   computed: {},
-  methods: {},
-  mounted() {},
-  beforeDestroy() {},
-  watch: {},
+  methods: {
+    onClickRedo() {
+      this.winNumbers = getWinNumbers();
+      this.winBalls = [];
+      this.bonus = null;
+      this.redo = false;
+
+      this.showBalls();
+    },
+
+    showBalls() {
+      for (let i = 0 ; i < this.winNumbers.length -1; i++) {
+        timeout[i] = setTimeout(() => {
+          this.winBalls.push(this.winNumbers[i]);
+        }, (i + 1) * 1000 );
+      };
+      
+      timeout[6] = setTimeout(() => {
+        this.bonus = this.winNumbers[6];
+        this.redo = true;
+      }, 7000);
+      }
+  },
+  mounted() {
+    this.showBalls();
+  },
+  beforeDestroy() {
+    timeout.forEach(() => {
+      clearTimeout(t);
+    });
+  },
+  watch: {  //값의 변경을 감지
+    bonus(val, oldVal) {
+      if(val.length === 0 ) {
+        this.showBalls();
+      }
+    }
+  },
 };
 </script>
   
