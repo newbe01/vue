4f425ac13023d0de88040188021d212b57956234
newbe01 <pkwaa3@gmail.com>
2023-11-06 12:40:22 +0900

    lotto game

---
+++ html/lotto/lottoGenerator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  beforeDestroy() {},
+  watch: {},
+};
+</script>
+  
+<style scoped>
+</style>
+  
---
+++ html/lotto/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import lottoGenerator from './lottoGenerator';
+
+new Vue(lottoGenerator).$mount('#root')
