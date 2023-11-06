b2537244cf37e7a6fefabe889e50e4e7a820d897
newbe01 <pkwaa3@gmail.com>
2023-11-06 11:09:08 +0900

    `v-bind`의 class, style 은 객체형식을 지원해줌

--- html/rockPaperScissors/rockPaperScissors.vue
+++ html/rockPaperScissors/rockPaperScissors.vue
@@ -1,12 +1,22 @@
 <template>
-
+  <div>
+    <div id = "computer" :style="{}"></div>
+    <div>
+      <button> rock </button>
+      <button> scissors </button>
+      <button> paper </button>
+    </div>
+    <div> {{ result }} </div>
+    <div> socre : {{ score }} </div>
+  </div>
 </template>
   
 <script>
 export default {
   data() {
     return {
-
+      result : "",
+      socre : 0,
     };
   },
   computed: {
@@ -19,7 +29,11 @@ export default {
 </script>
   
 <style scoped>
-
+  #computer {
+    width : 142px;
+    height : 200px;
+    background-position: 0 0;
+  }
 
 </style>
   
