aa576d0dc7983e7be64013adefd392edcad111dc
newbe01 <pkwaa3@gmail.com>
2023-11-07 10:44:28 +0900

    eventBus
    각 컴포넌트의 이번트들을 main vue에서 관리할 수 있도록 eventBus 를 구현

--- html/ticTacToe/tdComponent.vue
+++ html/ticTacToe/tdComponent.vue
@@ -4,6 +4,7 @@
 </template>
     
 <script>
+import eventBus from './eventBus';
     export default {
         props: {
             cellData: String,
@@ -16,48 +17,9 @@
         computed: {},
         methods: {
             onClickTd() {
-            if (this.cellData) return;
-
-            const rootData = this.$root.$data;
-            console.log(rootData);
-            this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
-
-            let win = false;
-            if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
-            win = true;
-            }
-            if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
-            win = true;
-            }
-            if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
-            win = true;
-            }
-            if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
-            win = true;
-            }
-
-            if (win) { // 이긴 경우: 3줄 달성
-            rootData.winner = rootData.turn;
-            rootData.turn = 'O';
-            rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
-            } else { // 무승부
-                let all = true; // all이 true면 무승부라는 뜻
-                rootData.tableData.forEach((row) => { // 무승부 검사
-                    row.forEach((cell) => {
-                    if (!cell) {
-                        all = false;
-                    }
-                    });
-                });
+                if (this.cellData) return;
                 
-                if (all) { // 무승부
-                    rootData.winner = '';
-                    rootData.turn = 'O';
-                    rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
-                } else {
-                    rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
-                }
-            }
+                eventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
             }
         },
         mounted() {},
--- html/ticTacToe/ticTacToe.vue
+++ html/ticTacToe/ticTacToe.vue
@@ -8,6 +8,7 @@
   
 <script>
 import tableComponent from "./tableComponent.vue";
+import eventBus from "./eventBus.js";
 
   export default {
     components: {
@@ -25,7 +26,54 @@ import tableComponent from "./tableComponent.vue";
       };
     },
     computed: {},
-    methods: {},
+    methods: {
+      onClickTd(rowIndex, cellIndex) {
+
+        // const this = this.$root.$data;
+            console.log(this);
+            this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+
+            let win = false;
+            if (this.tableData[rowIndex][0] === this.turn && this.tableData[rowIndex][1] === this.turn && this.tableData[rowIndex][2] === this.turn) {
+            win = true;
+            }
+            if (this.tableData[0][cellIndex] === this.turn && this.tableData[1][cellIndex] === this.turn && this.tableData[2][cellIndex] === this.turn) {
+            win = true;
+            }
+            if (this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn) {
+            win = true;
+            }
+            if (this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn) {
+            win = true;
+            }
+
+            if (win) { // 이긴 경우: 3줄 달성
+            this.winner = this.turn;
+            this.turn = 'O';
+            this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+            } else { // 무승부
+                let all = true; // all이 true면 무승부라는 뜻
+                this.tableData.forEach((row) => { // 무승부 검사
+                    row.forEach((cell) => {
+                    if (!cell) {
+                        all = false;
+                    }
+                    });
+                });
+                
+                if (all) { // 무승부
+                    this.winner = '';
+                    this.turn = 'O';
+                    this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+                } else {
+                    this.turn = this.turn === 'O' ? 'X' : 'O';
+                }
+            }
+      }
+    },
+    created() {
+      eventBus.$on('clickTd', this.onClickTd);
+    },
     mounted() {},
     beforeDestroy() {},
     watch: {
