17a9d69ca8f748b81bd8d14563f41b8a8cf2601d
newbe01 <pkwaa3@gmail.com>
2023-11-08 15:10:30 +0900

    vuex를 사용해도, 컴포넌트에서만 사용하는 data들은 각 컴포넌트에서 다룰수 있다.

---
+++ html/mineSweeper/mineForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="number" placeholder="row" value="row" @change="onChangeRow" />
+        <input type="number" placeholder="cell" value="cell" @change="onChangeCell" />
+        <input type="number" placeholder="mine" value="mine" @change="onChangeMine" />
+    </div>
+</template>
+    
+<script>
+    export default {
+        data() {
+            return {
+                row : 10,
+                cell : 10,
+                mine: 20,
+            };
+        },
+        methods : {
+            onChangeRow(e) {
+                this.row = e.target.value;
+            },
+            onChangeCell(e) {
+                this.cell = e.target.value;
+            },
+            onChangeMine(e) {
+                this.mine = e.target.value;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+</style>
